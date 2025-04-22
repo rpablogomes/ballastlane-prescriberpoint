0359c0bd9be73de235a0ca4041fc3461af6f6b10
R. Pablo Gomes <rpablo.gdr@gmail.com>
2025-04-22 19:59:02 -0300

    Revert "feat/to create a dailymed scrapper"

--- scrapper-and-llm/src/main.py
+++ scrapper-and-llm/src/main.py
@@ -1,17 +1,8 @@
 
 from fastapi import FastAPI
-from scrap.dailymed import dailymedscrap
 
 app = FastAPI()
 
-@app.get("/{drugName}")
-def read_root(drugName: str):
-    """"endpoint that return the value of the medicine"""
-    
-    ##I set Dupixent based on requiment. However, the intention is to get the medicine name from the user.
-
-    response = dailymedscrap("Dupixent")
-
-    for indications in response:
-        
-    return response
+@app.get("/")
+def read_root():
+    return {"message": "Hello Prescriber Point!"}
